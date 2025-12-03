459fe8eecdc1f870a95876dc3dba86fd6f635acd
GMKrishna99 <gmohankrishna23@gmail.com>
2025-12-03 18:27:26 +0530

    shadcd cn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">initial setup</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
 }
